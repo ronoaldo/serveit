5a2c9c71e01b733de6b8efda0a43118d91b5df61
Ronoaldo JLP <ronoaldo@gmail.com>
2015-04-07 02:46:48 +0000

    Added new shortcut versions

--- main.go
+++ main.go
@@ -18,13 +18,14 @@ func init() {
 	}
 
 	flag.StringVar(&port, "port", "8080", "Serve port number")
+	flag.StringVar(&port, "p", "8080", "Serve port number (shortcut)")
 	flag.StringVar(&dir, "dir", pwd, "Directory to be served")
+	flag.StringVar(&dir, "d", pwd, "Directory to be served (shortcut)")
 }
 
 func main() {
 	flag.Parse()
 	fmt.Println("Server start on:", port)
 	fmt.Println("Serving:", dir)
-
 	log.Fatal(http.ListenAndServe(":"+port, http.FileServer(http.Dir(dir))))
 }
